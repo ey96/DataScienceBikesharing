8c305ed16308271a8bd8072b4979e22856fa0815
Mo <63042732+t00m-dev@users.noreply.github.com>
2020-05-02 11:33:30 +0200

    adapt the structure from the PDS_Project

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='nextbike',
+    version='0.0.1dev1',
+    description="Programming Data Science",
+    author="Students",
+    author_email="blankspace@blankspace",
+    packages=["nextbike"],
+    install_requires=['pandas', 'scikit-learn', 'click'],
+    entry_points={
+        'console_scripts': ['nextbike=nextbike.cli:main']
+    }
+)
