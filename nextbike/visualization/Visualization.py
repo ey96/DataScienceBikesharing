8f36a19adc8bd317e9b30899cc8def19a338c53d
Mo <63042732+t00m-dev@users.noreply.github.com>
2020-05-18 16:46:24 +0200

    extend Visualization.py and updated setup.py

--- nextbike/visualization/Visualization.py
+++ nextbike/visualization/Visualization.py
@@ -49,7 +49,7 @@ def make_point(row):
     return Point(row.longitude_start, row.latitude_start)
 
 
-# task c
+# ab task c
 def create_map(shape="dortmund_plz.geojson", center=CONSTANTS.CENTER_OF_DORTMUND, tiles=CONSTANTS.TILES,
                attr=CONSTANTS.ATTR, zoom_start=12, min_zoom=11, height="100%", width="100%"):
 
@@ -65,3 +65,80 @@ def create_map(shape="dortmund_plz.geojson", center=CONSTANTS.CENTER_OF_DORTMUND
     folium.GeoJson(__read_geo_data(shape), name='geojson').add_to(map)
 
     return map
+
+
+def station_capacity(df):
+    tmp_map = create_map()
+    tmp_map.add_child(plugins.HeatMap(df["coordinates_start"], radius=20))
+    tmp_map.add_child(plugins.HeatMap(df["coordinates_end"], radius=20))
+
+    return tmp_map
+
+
+def most_used_station(df, amount=1000):
+    tmp_map = create_map()
+    mc = MarkerCluster()
+    df = shuffle(df)
+
+    i = 0
+    for index, row in df.iterrows():
+        if i <= amount:
+            mc.add_child(folium.Marker(location=[row["latitude_start"], row["longitude_start"]]))
+            mc.add_child(folium.Marker(location=[row["latitude_end"], row["longitude_end"]]))
+            tmp_map.add_child(mc)
+            i = i + 1
+        else:
+            break
+    return tmp_map
+
+
+def show_trips(df, amount=500):
+    tmp_map = create_map()
+
+    df = shuffle(df)
+
+    i = 0
+    for index, row in df.iterrows():
+        if i <= amount:
+            folium.ColorLine([row["coordinates_start"], row["coordinates_end"]],
+                             colors=[0, 1, 2],
+                             colormap=["blue", "green"],
+                             weight=1,
+                             opacity=0.3).add_to(tmp_map)
+            i = i + 1
+        else:
+            break
+    return tmp_map
+
+
+def show_map_at_specific_day(df, date="2019-01-20", street="Signal Iduna Park", coord=[]):
+    tmp_map = create_map()
+
+    if not coord:
+        try:
+            from geopy.geocoders import Nominatim
+        except ImportError as e:
+            print(e)
+
+    geo_locator = Nominatim(user_agent="http")
+    loc = geo_locator.geocode(street)
+
+    folium.Marker(location=[loc.latitude, loc.longitude],
+                  popup=loc,
+                  icon=folium.Icon(color='blue'),
+                  ).add_to(tmp_map)
+
+    df_tmp = df[(df['datetime_start'] >= date + " 00:00:00") & (df['datetime_start'] <= date + " 23:59:59")]
+
+    tmp_map.add_child(plugins.HeatMap(df_tmp["coordinates_start"], radius=20))
+    tmp_map.add_child(plugins.HeatMap(df_tmp["coordinates_end"], radius=20))
+
+    for index, row in df_tmp.iterrows():
+        folium.ColorLine([row["coordinates_start"], row["coordinates_end"]],
+                         colors=[0, 1, 2],
+                         colormap=["red", "blue"],
+                         weight=1,
+                         opacity=0.5).add_to(tmp_map)
+
+    return tmp_map
+
