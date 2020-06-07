72d0291178622533f22d1479b9b23f9ea66c92ee
Mo <63042732+t00m-dev@users.noreply.github.com>
2020-06-08 00:18:01 +0200

    - extend random forest model

--- nextbike/model/ensemble/random_forest.py
+++ nextbike/model/ensemble/random_forest.py
@@ -8,45 +8,92 @@ from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import time
 
+# arrays for results
+model, r2, rmse, mae, exetime, desc = [], [], [], [], [], []
 
-def __init__(df):
+dic = {
+    'name': model,
+    'r2': r2,
+    'rmse': rmse,
+    "mae": mae,
+    "exetime": exetime,
+    "desc": desc
+}
+
+
+def __init__(df, log=False):
     # best features so far
     X = df[["month", "weekday", "day_of_year", "hour", "minute", "latitude_start", "longitude_start",
             "area_start", "temperature °C", "precipitation", "distanceToUniversity", "distanceToCentralStation"]]
 
-    y = df["trip_duration"]
+    if log is True:
+        df["log_duration"] = df["trip_duration"].transform(np.log)
+        y = df["log_duration"]
+    else:
+        y = df["trip_duration"]
+
     # split data / cross validation
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # scaling the data
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+
+    scaler.fit(X_test)
+    X_test_scaled = scaler.transform(X_test)
 
     return {
         'X_train': X_train,
         'X_test': X_test,
         'y_train': y_train,
-        'y_test': y_test
+        'y_test': y_test,
+        'X_train_scaled': X_train_scaled,
+        'X_test_scaled': X_test_scaled
     }
 
 
-def train(init):
+def rfr(init, estimator=RandomForestRegressor()):
+    # first try rfr
     start = time.time()  # to measure execution time
 
-    # scaling the data
-    scaler = StandardScaler()
-    scaler.fit(init['X_train'])
-    X_train_scaled = scaler.transform(init['X_train'])
+    # fitting a random forrest regressor
+    rfr = estimator
+    rfr.fit(init['X_train_scaled'], init['y_train'])
+    pred = rfr.predict(init['X_test_scaled'])
+    pred_train = rfr.predict(init['X_train_scaled'])
 
-    scaler.fit(init['X_test'])
-    X_test_scaled = scaler.transform(init['X_test'])
+    end = time.time()
+    execution_time = (end - start) / 6
+
+    _get_results(rfr, init['X_train_scaled'], pred_train, init['X_test_scaled'], pred, init)
+
+    model.append("RF")
+    r2.append(rfr.score(init['X_test_scaled'], init['y_test']))
+    rmse.append(np.sqrt(mean_squared_error(init['y_test'], pred)))
+    mae.append(mean_absolute_error(init['y_test'], pred))
+    exetime.append(execution_time)  # save execution time from the cell above
+    desc.append("first try rfr, overfitted ")  # describe the model above
+
+    return {
+        'pred': pred,
+        'pred_train': pred_train
+    }
+
+
+# best model
+def train(init):
+    start = time.time()  # to measure execution time
 
     # fitting a random forrest regressor
     rfr = RandomForestRegressor(max_features="auto", n_estimators=1155, max_depth=70, min_samples_split=10,
                                 min_samples_leaf=8, bootstrap=True)
     print('init training parameters')
-    rfr.fit(X_train_scaled, init['y_train'])
-    pred = rfr.predict(X_test_scaled)
-    pred_train = rfr.predict(X_train_scaled)
+    rfr.fit(init['X_train_scaled'], init['y_train'])
+    pred = rfr.predict(init['X_test_scaled'])
+    pred_train = rfr.predict(init['X_train_scaled'])
     print('Training done')
 
-    _get_results(rfr, X_train_scaled, pred_train, X_test_scaled, pred, init)
+    _get_results(rfr, init['X_train_scaled'], pred_train, init['X_test_scaled'], pred, init)
     end = time.time()
     execution_time = (end - start) / 60
 
@@ -98,3 +145,28 @@ def _get_results(model, X_train_scaled,pred_train,X_test_scaled, pred, init):
     print("R2-Score is: {}".format(model.score(X_test_scaled, init['y_test'])))
     print("RMSE: {}".format(np.sqrt(mean_squared_error(init['y_test'], pred))))
     print("MAE: {}".format(mean_absolute_error(init['y_test'], pred)))
+
+
+def convert_log_to_exp(init, rfr):
+    # summary of results
+    y_train = np.exp(init['y_train'])
+    pred_train = np.exp(rfr['pred_train'])
+
+    y_test = np.exp(init['y_test'])
+    pred = np.exp(rfr['pred'])
+
+    print("w/o cross-validation:")
+    print("RMSE: {}".format(np.sqrt(mean_squared_error(y_train, pred_train))))
+    print("MAE: {}".format(mean_absolute_error(y_train, pred_train)))
+    print("")
+    print("w/ cross-validation")
+    print("RMSE: {}".format(np.sqrt(mean_squared_error(y_test, pred))))
+    print("MAE: {}".format(mean_absolute_error(y_test, pred)))
+
+    model.append("RF")
+    r2.append("-")
+    rmse.append(np.sqrt(mean_squared_error(y_test, pred)))
+    mae.append(mean_absolute_error(y_test, pred))
+    exetime.append(0)
+    desc.append("using log of trip duration/ exp after prediction")
+
